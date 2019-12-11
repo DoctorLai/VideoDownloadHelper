8de3487fe7ca9f9b0e5d9c0b1baf0b978a1dc3d4
justyy <dr.zhihua.lai@gmail.com>
2019-12-11 21:32:08 +0000

    Fix

--- video-url-parser/js/constants.js
+++ video-url-parser/js/constants.js
@@ -3,6 +3,20 @@
 
 const max_url_length = 95;
 
+const steemit_domains = [
+    'steempeak.com',
+    'steemit.com', 
+    'steemkr.com', 
+    'cnsteem.com',
+    'utopian.io',
+    'staging.busy.org',
+    'steemhunt.com',
+    'mspsteem.com',
+    'steemdb.com',
+    'busy.org',
+    'steemd.com'
+];   
+
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
 	module.exports = {
 		max_url_length, steemit_domains
